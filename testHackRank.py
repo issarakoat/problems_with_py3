069706636a8f3ba3e35b5724f3c454e372960db1
issarakoat <weeravut.issarakoat@gmail.com>
2020-02-16 13:51:06 -0700

    yo

--- testHackRank.py
+++ testHackRank.py
@@ -22,13 +22,3 @@ assert(HackRank.rotLeft(arr, 4)==[5, 1, 2, 3, 4,])
 assert(HackRank.rotLeft(arr1, 10)==[77, 97, 58, 1, 86, 58, 26, 10, 86, 51, 41, 73, 89, 7, 10, 1, 59, 58, 84, 77])
 assert(HackRank.rotLeft(arr2, 13)==[87, 97, 33, 47, 70, 37, 8, 53, 13, 93, 71, 72, 51, 100, 60])
 
-
-# test = 30000
-# factorial_recur = HackRank.factorial_recur(test)
-# print(factorial_recur)
-
-# factorial_iter = HackRank.factorial_iter(test)
-# print(factorial_iter)
-
-test = 900
-print(HackRank.sum_recur(test))
