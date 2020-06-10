cd03a774bceae3586d84b2a42ca8ae38dfbdfd22
issarakoat <weeravut.issarakoat@gmail.com>
2020-06-10 12:32:47 -0600

    added fibonacci tests

--- dynamic-programming/fibonacci.py
+++ dynamic-programming/fibonacci.py
@@ -8,6 +8,11 @@ def fibonacci(n):
         c = a + b
         a = b
         b = c
-        print(c)
     return a + b
-print(fibonacci(7))
+assert(fibonacci(1) == 1)
+assert(fibonacci(2) == 1)
+assert(fibonacci(3) == 2)
+assert(fibonacci(4) == 3)
+assert(fibonacci(5) == 5)
+assert(fibonacci(6) == 8)
+assert(fibonacci(7) == 13)
